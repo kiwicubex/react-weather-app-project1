4eb2a62dfd1d0e3fec70475e4c88f332a5c55dc1
kiwicubex <86155059+kiwicubex@users.noreply.github.com>
2021-12-06 23:56:12 +0800

    Removed unnecessary files and added Github repo Url

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://brave-meninsky-cd508d.netlify.app/about.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Amanda Chan{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/kiwicubex/react-weather-app-project1"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
