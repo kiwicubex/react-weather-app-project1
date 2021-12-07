ef3d7976800d9d74f61002912674364ad3832589
kiwicubex <86155059+kiwicubex@users.noreply.github.com>
2021-12-07 19:55:35 +0800

    added a default city

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import "./Weather.css";
 import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props) {
   return (
@@ -18,12 +19,9 @@ export default function WeatherInfo(props) {
             <div className="float-left">
               <WeatherIcon code={props.data.icon} />
             </div>
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-            />
-            <WeatherTemperature celsius={props.data.temperature} />
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
           </div>
         </div>
         <div className="col-6">
