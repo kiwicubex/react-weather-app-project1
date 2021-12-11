1781a6e5682396715746b12e2dc1e2d1b88128f0
kiwicubex <86155059+kiwicubex@users.noreply.github.com>
2021-12-11 17:06:10 +0800

    fixed the array-callback-return error and made forecast applicable with any city input

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
@@ -7,6 +7,9 @@ import axios from "axios";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
   function handleResponse(response) {
     console.log(response.data);
     setForecast(response.data.daily);
@@ -23,6 +26,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
